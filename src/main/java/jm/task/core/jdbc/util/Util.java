b6126c7b781e13ab379f0bbc3f25b649059b60fa
Denis Doich <doich225@gmail.com>
2022-07-17 21:42:17 +0300

    JDBS added

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,25 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    public static final String DB_DRIVER = "com.mysql.cj.dbc.Driver";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "controll";
+public Connection getConnection() throws SQLException {
+Connection connection = null;
+try {
+    connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+    System.out.println("Connection OK");
+    connection.setAutoCommit(false);
+} catch (SQLException e) {
+    e.printStackTrace();
+    System.out.println("Connection ERROR");
+}
+    return connection;
+}
+
+}
